a573e553c4a3e3274b4dda4d40263322cba402a1
zephyr <shejiayu1998@qq.com>
2020-03-01 22:38:01 +0800

    Fix

--- assignment_1/WebServer-A0188314B.py
+++ assignment_1/WebServer-A0188314B.py
@@ -2,22 +2,17 @@ import sys
 from socket import *
 
 class HTTPRequest:
-    def __init__(self, input_request):
-        substrings = input_request.split()
-        self.method = substrings[0].upper().decode()
-        self.path = substrings[1].decode()
+    def __init__(self, request):
+        self.method = request[0]
+        self.path = request[1]
         if (self.method == 'POST'):
-            self.content_length = substrings[-2].decode()
-            self.content_body = substrings[-1]
+            self.content_body = request[2]
     
     def get_method(self):
         return self.method
     
     def get_path(self):
         return self.path
-
-    def get_content_length(self):
-        return self.content_length
     
     def get_content_body(self):
         return self.content_body
@@ -25,98 +20,94 @@ class HTTPRequest:
 class WebServer:
     def __init__(self, port_number):
         self.port_number = port_number
-        self.buffer_input = bytearray()
-        self.buffer_output = bytearray()
         self.current_request = bytearray()
-        self.current_request_count = 0
-        self.current_content_length = 0
-        self.current_content_count = 0
-        self.parsing_content = False
         self.hashtable = dict()
 
     def start(self):
-        listening_socket = socket(AF_INET, SOCK_STREAM)
-        listening_socket.bind(('', self.port_number))
-        listening_socket.listen(1)
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind(('', self.port_number))
+        self.socket.listen(1)
         sys.stderr.write('[Server] Listening on port ' + str(self.port_number) + '.\n')
         while True:
-            connection_socket = listening_socket.accept()
+            self.connection_socket, address = self.socket.accept()
             sys.stderr.write('[Server] Connected to client.\n')
-            self.buffer_input = connection_socket.recv(1024)
-            while len(self.buffer_input) > 0:
-                self.extract_request(connection_socket)
-                self.buffer_input = connection_socket.recv(1024)
-        listening_socket.close()
+            while True:
+                input_data = self.connection_socket.recv(1024)
+                self.current_request += input_data
+                if len(input_data) == 0 and len(self.current_request) == 0:
+                    break
+                self.parse_request()
+        self.socket.shutdown(SHUT_RDWR)
+        self.socket.close()
 
-    def extract_request(self, connection_socket):
-        for i in range(len(self.buffer_input)):
-            self.current_request.append(self.buffer_input[i])
-            self.current_request_count += 1
-            if (self.current_request_count > 1) and (self.current_request[-1] == ord(' ')) and (self.current_request[-2] == ord(' ')):
-                if (self.current_request.startswith(('POST').encode())):
-                    # ?
-                    self.current_content_length = int(chr(self.current_request[-3]))
-                    self.parsing_content = True
+    def parse_request(self):
+        while True:
+            header_end = self.current_request.find(('  ').encode())
+            if header_end == -1:
+                break
+            substrings = self.current_request[:header_end].decode().split()
+            content_exist = False
+            content_length = 0
+            for i in range(len(substrings)):
+                if substrings[i].lower() == 'Content-Length'.lower() :
+                    try:
+                        content_exist = True
+                        content_length = int(substrings[i + 1])
+                        break
+                    except ValueError:
+                        continue
+            request = [substrings[0].upper(), substrings[1]]
+            if content_exist:
+                if header_end + 2 + content_length > len(self.current_request):
+                    break
                 else:
-                    self.handle_request(connection_socket)
-                    self.current_request = bytearray()
-                    self.current_request_length = 0
+                    request.append(self.current_request[header_end + 2 : header_end + 2 + content_length])
+                    self.process_request(HTTPRequest(request))
+                    self.current_request = self.current_request[header_end + 2 + content_length:]
+            else:
+                self.process_request(HTTPRequest(request))
+                self.current_request = self.current_request[header_end + 2:]
 
-            if self.parsing_content == True:
-                if self.current_content_count == self.current_content_length:
-                    self.handle_request(connection_socket)
-                    self.current_request = bytearray()
-                    self.current_request_length = 0
-                    self.current_content_count = 0
-                    self.parsing_content = False
-                self.current_content_count += 1
-
-    def handle_request(self, connection_socket):
-        request = HTTPRequest(self.current_request)
-
+    def process_request(self, request):
         method = request.get_method()
         path = request.get_path()
-        
         if path.startswith('/key/'):
             key = path.replace('/key/', '')
             if method == 'POST':
                 self.hashtable[key] = request.get_content_body()
-                self.send_200(connection_socket, -1)
+                self.send_response(200)
             elif method == 'GET':
                 if key in self.hashtable:
-                    self.buffer_output += self.hashtable[key]
-                    self.send_200(connection_socket, len(self.hashtable[key]))
+                    self.send_response(200, self.hashtable[key])
                 else:
-                    self.send_404(connection_socket)
+                    self.send_response(404)
             elif method == 'DELETE':
                 if key in self.hashtable:
+                    self.send_response(200, self.hashtable[key])
                     del self.hashtable[key]
-                    self.send_200(connection_socket, len(self.hashtable[key]))
                 else:
-                    self.send_404(connection_socket)
+                    self.send_response(404)
             else:
                 sys.stderr.write('[Server] Unknown request method.\n')
         else:
             sys.stderr.write('[Server] Unknown request path.\n')
 
-    def send_404(self, connection_socket):
-        response = ('404 NotFound  ').encode()
-        connection_socket.send(response)
-
-    def send_200(self, connection_socket, content_length):
+    def send_response(self, code, content_body = bytearray()):        
         response = bytearray()
-        if content_length > 0:
-            response = ('200 OK content-length ' + str(content_length) + '  ').encode() + self.buffer_output
+        if code == 404:
+            response = ('404 NotFound  ').encode()
         else:
-            response = ('200 OK  ').encode()
-        connection_socket.send(response)
-        self.buffer_output = bytearray()
+            if len(content_body) > 0:
+                response = ('200 OK content-length ' + str(len(content_body)) + '  ').encode() + content_body
+            else:
+                response = ('200 OK  ').encode()
+        self.connection_socket.send(response)
 
 def main():
     if len(sys.argv) != 2:
         sys.stderr.write("Usage: python WebServer-A0188314B.py port_number\n")
         sys.stderr.write("Example: python WebServer-A0188314B.py 1024\n")
-        exit(0)
+        return
     WebServer(int(sys.argv[1])).start()
 
 if __name__ == "__main__":
